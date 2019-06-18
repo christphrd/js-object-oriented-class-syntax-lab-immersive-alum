c006f2d850a62a687f90d2270b9804ad2525891a
Christopher Diep <diep.christopher@gmail.com>
2019-06-18 16:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,12 @@ class BoardMember {
   doCharity() {
     return "I like to help people."
   }
+  
+  releasePressStatement() {
+    return "You will see great things from Scuber."
+  }
+  
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and `
+  }
 }
